e3cc1c6c5129360bac30dbf76cc5bc012ca966c3
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 20:45:59 -0500

    Add an api/config to test

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// Add an api/config to test
+
+app.get("/api/config", (req,res) =>{
+    console.log({success:true});
+})
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
