9c5642038a645af40d3adfe09194b6e898c5324e
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 20:42:43 -0500

    Added listen on PORT and the middleware.

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const mongoose = require ("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 3001
+
+// Add middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
