76cc1c0c60b3fcc6770859fea97f7d7ee5a9408f
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 22:44:09 -0500

    Added imported the navbar in app.js and browserrouter, route and switch. Added the route for the navbar links.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import react from "react";
+import { render } from "react-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-route-dom";
+import NavBar from "./components/Navbar/NavBar";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  render (){
+     return (
+       <Router>
+         <div>
+          <NavBar/>
+          <Switch>
+<Route exact path ="/" components={Search} />
+<Route exact path ="/search" components={Search} />
+<Route exact path ="/saved" components={Saved} />
+          </Switch>
+         </div>
+
+
+       </Router>
+   
+     
+  
   );
+  }
+ 
 }
 
 export default App;
