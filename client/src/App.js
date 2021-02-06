346953ca770dca9cce0b2c49fc3e8ed59b12c48d
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 23:12:51 -0500

    Created a containers folder in src. Added search and saved jsx files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
-import react from "react";
-import { render } from "react-dom";
-import {BrowserRouter as Router, Route, Switch} from "react-route-dom";
+
+import { Component } from "react";
+import {BrowserRouter as Router,Switch} from "react-router-dom";
 import NavBar from "./components/Navbar/NavBar";
 
 
 function App() {
-  render (){
      return (
        <Router>
          <div>
           <NavBar/>
           <Switch>
-<Route exact path ="/" components={Search} />
-<Route exact path ="/search" components={Search} />
-<Route exact path ="/saved" components={Saved} />
           </Switch>
          </div>
-
-
        </Router>
-   
-     
-  
   );
-  }
- 
+
 }
 
 export default App;
