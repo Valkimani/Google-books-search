04f00d476c1cfd656f3807c5de05cd565133f785
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 20:49:03 -0500

    Add mongoDB and mongoose.

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Add mongoDB and mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/google_books_search",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
+  
+  const connection = mongoose.connection;
+  
+  connection.on("connected", () => {
+    console.log("Mongoose successfully connected!");
+  });
+  
+  connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+  });
+
 
 // Add an api/config to test
 
