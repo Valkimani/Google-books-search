8a50c998016cac7256f86bb282f116df29abd59c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 23:41:14 -0500

    Installed react router in the client file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 
 import { Component } from "react";
-import {BrowserRouter as Router,Switch} from "react-router-dom";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import NavBar from "./components/Navbar/NavBar";
-
+import Save from "./containers/Save/Save";
+import Search from "./containers/Search/Search";
 
 function App() {
      return (
-       <Router>
-         <div>
+       <div>
+          <Router>
           <NavBar/>
           <Switch>
+            <Route exact path="/" components={Search}/>
+            <Route exact path="/save" components={Save}/>
+            <Route exact path="/search" components={Search}/>
           </Switch>
-         </div>
        </Router>
-  );
-
+  </div>
+); 
 }
 
 export default App;
