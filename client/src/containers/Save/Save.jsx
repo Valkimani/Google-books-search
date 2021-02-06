263fe01a7bbe2363a41dff157df5a76d80c3cea1
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 23:17:57 -0500

    Added containers in both search and save files.

---
+++ client/src/containers/Save/Save.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Save = () => {
+    return (
+        <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1 className="center-align">
+Saved books.
+                </h1>
+            </div>
+
+        </div>
+        
+    </div>
+);
+};
+
+export default Save;
---
+++ client/src/containers/Search/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="center-align">
+Searched books.
+                    </h1>
+                </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Search;
