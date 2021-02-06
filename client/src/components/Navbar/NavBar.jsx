98770c75500b6dc70b321f31794e7e849e377a1f
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-05 22:28:42 -0500

    Added a navbar.

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
-    );
-};
+
+        <ul className="nav">
+  <li className="nav-item">
+    <link className="nav-link active" href="#">Search</link>
+  </li>
+  <li className="nav-item">
+    <link className="nav-link" href="#">Saved</link>
+  </li>
+</ul>
+  );
+}
 
 export default NavBar;
