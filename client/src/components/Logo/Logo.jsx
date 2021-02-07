2a06ba7a1673ff5553d3752e169bb7c22dc5fa37
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-06 23:08:05 -0500

    Added a book folder in components.

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+        <img className="logo-position" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/256/Play-Books-icon.png" />
+        <h1 className="logo-text logo-position">Google Play Books Search </h1>
+    </div>
+    );
+};
+export default Logo;
--- client/src/pages/Save/Save.jsx
+++ client/src/pages/Save/Save.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Save = () => {
     return (
         <div>
